7325be9dd187e55082e027e514caab9109296135
pinakinc <chaubalpinakin@gmail.com>
2018-12-09 12:20:23 +0530

    Create paralleldemo.java

---
+++ paralleldemo.java
@@ -0,0 +1,21 @@
+public class paralleldemo {
+	@Test
+	public void google1() throws Exception {
+		BrowserFactory browserFactory = BrowserFactory.getInstance();
+		browserFactory.setDriver("chrome");
+		browserFactory.getDriver().get("http://www.google.com");
+		browserFactory.getDriver().findElement(By.name("q"))
+				.sendKeys("Selenium");
+		Thread.sleep(3000);
+		Actions act = new Actions(browserFactory.getDriver());
+		// browserFactory.getDriver().findElement(By.name("btnK")).click();
+		act.sendKeys(browserFactory.getDriver().findElement(By.name("q")),
+				Keys.TAB).build().perform();
+		Thread.sleep(3000);
+		browserFactory.getDriver().findElements(By.name("btnK")).get(1).click();
+		// act.keyUp(Keys.ENTER);
+		Thread.sleep(8000);
+		browserFactory.getDriver().quit();
+	}
+
+}
